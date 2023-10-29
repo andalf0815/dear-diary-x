f8396415e94a4586c2b151dc947ec33b6501e8a9
Andreas Gruber <andreas-gruber@outlook.at>
2023-10-29 07:52:26 +0100

    Created simple page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navigation from './layout/Navigation';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigation />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/layout/Navigation.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <>
+      <div>
+        <h1>Navigation</h1>
+      </div>
+      <Outlet />
+    </>
+  );
+}
+
+export default Navigation;
