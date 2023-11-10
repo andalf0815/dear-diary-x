63bb57cad012dcb2296101113d00ffe393dbbfeb
Andreas Gruber <andreas-gruber@outlook.at>
2023-11-10 20:56:08 +0100

    Added a more precise time ago text for the memories

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,15 +1,32 @@
+import { DateTime } from 'luxon';
+
 function MemoryCard(props) {
   const memory = props.memory;
 
-  const today = new Date();
-  const memoryDate = new Date(memory.memoryDate);
-  const diffInDays = Math.floor((today.getTime() - memoryDate.getTime()) / (1000 * 3600 * 24));
+  const today = DateTime.now().startOf('day');
+  const memoryDate = DateTime.fromISO(memory.memoryDate).startOf('day');
+  const diffInDays = today.diff(memoryDate, 'days').toObject().days;
+  const diffInYears = today.diff(memoryDate, 'years').toObject().years;
+
+  let timeAgoText = '';
+
+  if (diffInDays <= 1) {
+    timeAgoText = `${diffInDays < 1 ? 'Today' : 'Yesterday'}`;
+  } else if (diffInDays < 8) {
+    timeAgoText = `${diffInDays} day${diffInDays !== 1 ? 's' : ''} ago`;
+  } else if (today.minus({ months: 1 }).hasSame(memoryDate, 'day')) {
+    timeAgoText = '1 month ago';
+  } else if (diffInYears >= 1 && today.minus({ years: Math.floor(diffInYears) }).hasSame(memoryDate, 'day')) {
+    timeAgoText = Math.floor(diffInYears) + ` year${diffInYears !== 1 ? 's' : ''} ago`;
+  } else {
+    timeAgoText = 'ka';
+  }
 
   return (
     <div id='memory-container' className='max-w-[60rem] w-1/2 lg:min-w-full p-3 snap-center'>
       <div id='memory' className='flex flex-col h-[20rem] p-4 border-slate-400 border-2 rounded-md shadow-lg'>
         <p>
-          {diffInDays !== 0 ? `${diffInDays} days ago, on` : 'Today, on'} {memory.memoryDate}
+          {timeAgoText} on {memory.memoryDate}
         </p>
         <h1>
           {memory.emotion} {memory.title}
