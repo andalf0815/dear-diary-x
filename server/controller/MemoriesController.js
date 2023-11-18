4256003d47d41888159b439cff07c9dc800db10e
Andreas Gruber <andreas-gruber@outlook.at>
2023-11-18 21:26:21 +0100

    Implemented sorting of the memories

--- server/controller/MemoriesController.js
+++ server/controller/MemoriesController.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 // Get all memory
 const getAllMemories = async (req, res) => {
   try {
-    const memories = await Memory.find({});
+    const memories = await Memory.find({}).sort({memoryDate: -1});
     res.json(memories);
   } catch (err) {
     console.log(err);
